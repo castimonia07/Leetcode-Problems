c3a72d0d3970c09012468dadf20ec268f86825d8
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2024-10-10 01:17:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (81.11%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        // if(digits[n-1]<9){
+        //     digits[n-1]+=1;
+        // }
+        // else if(digits[n-1]==9){
+        //     digits.pop_back();
+        //     digits.push_back(0);
+        // }
+        // return digits;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
