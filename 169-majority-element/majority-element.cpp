39369955e104bfe2ae56cd19d7b6f3be4095ce85
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2024-10-10 14:38:25 +0530

    Time: 18 ms (53.77%) | Memory: 27.1 MB (49.78%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     int cnt=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]==nums[i]){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt>n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        int n=nums.size();
+        map<int , int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second>n/2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
