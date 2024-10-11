9666321ad261615ab09b0c686a7aede43051ac7e
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2024-10-11 19:20:45 +0530

    Time: 19 ms (49.47%) | Memory: 26.9 MB (80.25%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -16,37 +16,37 @@ public:
         // return -1;
 
 
-        // int n=nums.size();
-        // map<int , int> mpp;
-        // for(int i=0;i<n;i++){
-        //     mpp[nums[i]]++;
-        // }
-        // for(auto it:mpp){
-        //     if(it.second>n/2){
-        //         return it.first;
-        //     }
-        // }
-        // return -1;
-
         int n=nums.size();
-        int cnt=0;
-        int el;
-
+        map<int , int> mpp;
         for(int i=0;i<n;i++){
-            if(cnt==0){
-                cnt=1;
-                el=nums[i];
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second>n/2){
+                return it.first;
             }
-            else if(el==nums[i]) cnt++;
-            else cnt--;
         }
-        int cnt1=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==el) cnt1++;
-        }
-        if(cnt1> n/2) return el;
-
         return -1;
+
+        // int n=nums.size();
+        // int cnt=0;
+        // int el;
+
+        // for(int i=0;i<n;i++){
+        //     if(cnt==0){
+        //         cnt=1;
+        //         el=nums[i];
+        //     }
+        //     else if(el==nums[i]) cnt++;
+        //     else cnt--;
+        // }
+        // int cnt1=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==el) cnt1++;
+        // }
+        // if(cnt1> n/2) return el;
+
+        // return -1;
 
 
 
