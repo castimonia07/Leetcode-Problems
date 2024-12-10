6722a833827a5ddb76f0f606e16baee82198b8de
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2024-12-11 01:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (91.42%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt=0;
+        for(int i=0;i<details.size();i++){
+            if(((details[i][11]-'0')*10+(details[i][12]-'0'))>60){
+                cnt++;
+            }
+        } 
+        return cnt;  
+    }
+};
