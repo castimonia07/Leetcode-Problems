1e9a96788969813c8ed703023aae032b81d0cee9
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2024-12-20 15:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (64.96%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> f;
+        int result;
+        int m=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            result=prices[i];
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[j]<=prices[i]){
+                    result=prices[i]-prices[j];
+                    break;
+                }
+            }
+            result=min(m,result);
+            f.push_back(result);
+
+        }
+        return f;
+    }
+};
