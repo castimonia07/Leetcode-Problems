7e399171f69e1bd9f103d3950d60d7d38015ac1a
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2024-12-21 22:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (19.84%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -19,7 +19,24 @@ public:
         //     i--;
         // }
         // return i;
-        long long n=sqrt(x);
-        return n;
+
+        // long long n=sqrt(x);
+        // return n;
+
+        if(x==0) return x;
+        int low=1,high=x;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(mid==x/mid){
+                return mid;
+            }
+            else if(mid<x/mid){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
     }
 };
