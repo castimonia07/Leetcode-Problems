117d4c1cc9f42c732abd620cc01820fe863dcd7e
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-01-11 15:47:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (8.09%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -28,14 +28,27 @@ public:
         // return head;
 
 
-        ListNode* temp=head;
-        ListNode* prev=NULL;
-        while(temp!=NULL){
-            ListNode* front=temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=front;
+        // ListNode* temp=head;
+        // ListNode* prev=NULL;
+        // while(temp!=NULL){
+        //     ListNode* front=temp->next;
+        //     temp->next=prev;
+        //     prev=temp;
+        //     temp=front;
+        // }
+        // return prev;
+
+        if(head==NULL || head->next==NULL ){
+            return head;
         }
-        return prev;
+        ListNode* newHead= reverseList(head->next);
+
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newHead;
+
+
+
     }
 };
