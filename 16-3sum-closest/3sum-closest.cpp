5205cf69c8ebf9e64d5d5cadd7db2d0901b54cd8
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-04-05 20:36:01 +0530

    Time: 7 ms (88.27%) | Memory: 14.1 MB (27.00%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -17,17 +17,47 @@ public:
         
         // return cs;
 
-        int cs=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                    int sum=nums[i]+nums[j]+nums[k];
-                    if(abs(sum-target)<abs(cs-target)){
-                        cs=sum;
-                    }
+        // int cs=nums[0]+nums[1]+nums[2];
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         for(int k=j+1;k<nums.size();k++){
+        //             int sum=nums[i]+nums[j]+nums[k];
+        //             if(abs(sum-target)<abs(cs-target)){
+        //                 cs=sum;
+        //             }
+        //         }
+        //     }
+        // }
+        // return cs;
+//         class Solution {
+// public:
+//     int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int cs = nums[0] + nums[1] + nums[2]; // initial closest sum
+
+        for(int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(abs(sum - target) < abs(cs - target)) {
+                    cs = sum;
+                }
+
+                if(sum < target) {
+                    left++;
+                }
+                else if(sum > target) {
+                    right--;
+                }
+                else {
+                    return sum; // exact match
                 }
             }
         }
+
         return cs;
     }
 };
