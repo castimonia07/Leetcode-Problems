f7929b19864472e2a716594c4d25a9e28e2e8c86
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-05-04 16:36:18 +0530

    Time: 3 ms (7.48%) | Memory: 9.8 MB (7.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
+        stack<int> st;
+        for(char c: s){
+            if(c=='(' || c=='[' || c=='{'){
                 st.push(c);
-            } else if (c == ')' || c == ']' || c == '}') {
-                if (st.empty()) return false;
-                
-                char top = st.top();
+            }
+            else if(c==')' || c==']' || c=='}'){
+                if(st.empty()){
+                    return false;
+                }
+                char top= st.top();
                 st.pop();
-                
-                if ((c == ')' && top != '(') ||
-                    (c == ']' && top != '[') ||
-                    (c == '}' && top != '{')) {
+                if(c==')' && top!='(' || c==']' && top!='[' || c=='}' && top!='{' ){
                     return false;
                 }
             }
         }
-    
-    return st.empty();
-}
-    
+        return (st.empty());
+        
+    }
 };
