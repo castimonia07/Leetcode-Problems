94ec6f520b80c679dea56d77190338e5a3e9a810
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-05-04 16:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (7.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     bool isValid(string s) {
+        // stack<int> st;
+        // for(char c: s){
+        //     if(c=='(' || c=='[' || c=='{'){
+        //         st.push(c);
+        //     }
+        //     else if(c==')' || c==']' || c=='}'){
+        //         if(st.empty()){
+        //             return false;
+        //         }
+        //         char top= st.top();
+        //         st.pop();
+        //         if(c==')' && top!='(' || c==']' && top!='[' || c=='}' && top!='{' ){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return (st.empty());
+        int n=s.size();
+        map<int ,int> mp;
+        mp['(']=+1;
+        mp['[']=+2;
+        mp['{']=+3;
+        mp[')']=-1;
+        mp[']']=-2;
+        mp['}']=-3;
+
         stack<int> st;
-        for(char c: s){
-            if(c=='(' || c=='[' || c=='{'){
-                st.push(c);
+        for(auto c:s){
+            if(mp[c]>0) st.push(c);
+            else{
+                if(st.empty() || mp[c]+mp[st.top()]!=0){return false;}
+                st.pop(); 
             }
-            else if(c==')' || c==']' || c=='}'){
-                if(st.empty()){
-                    return false;
-                }
-                char top= st.top();
-                st.pop();
-                if(c==')' && top!='(' || c==']' && top!='[' || c=='}' && top!='{' ){
-                    return false;
-                }
-            }
+
         }
-        return (st.empty());
+        return(st.empty());
+
         
     }
 };
