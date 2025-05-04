123d30a0388268a5e9c1e954125802fad3b7c5de
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-05-04 18:10:56 +0530

    Time: 47 ms (16.05%) | Memory: 12.5 MB (98.20%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // int i=0;
-        // while(i<static_cast<int>(s.length())-1){
-        //     if(s[i]==s[i+1]){
-        //         s.erase(i,2);
-        //         if(i>0) i--;
-        //         else i=0;
-        //     }
-        //     else{i++;}
-        // }
-        // return s;
+        int i=0;
+        while(i<static_cast<int>(s.length())-1){
+            if(s[i]==s[i+1]){
+                s.erase(i,2);
+                if(i>0) i--;
+                else i=0;
+            }
+            else{i++;}
+        }
+        return s;
 
-        stack<int> st;
-        for(char c:s){
-            if(!st.empty() && st.top()==c){
-                st.pop();
-            }
-            else{
-                st.push(c);
-            }
-        }
-        string t="";
-        while(!st.empty()){
-            t+=st.top();
-            st.pop();
-        }
-        reverse(t.begin(),t.end());
-        return t;
+        // stack<int> st;
+        // for(char c:s){
+        //     if(!st.empty() && st.top()==c){
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(c);
+        //     }
+        // }
+        // string t="";
+        // while(!st.empty()){
+        //     t+=st.top();
+        //     st.pop();
+        // }
+        // reverse(t.begin(),t.end());
+        // return t;
 
     }
 };
