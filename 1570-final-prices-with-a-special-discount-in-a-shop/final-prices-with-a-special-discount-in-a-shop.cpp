e105d97497a73fb82298c51111346a06273b1e75
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-05-07 08:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (27.36%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> f;
-        int result;
-        // int m=INT_MAX;
-        for(int i=0;i<prices.size();i++){
-            result=prices[i];
-            for(int j=i+1;j<prices.size();j++){
-                if(prices[j]<=prices[i]){
-                    result=prices[i]-prices[j];
-                    break;
-                }
+        // vector<int> f;
+        // int result;
+        // // int m=INT_MAX;
+        // for(int i=0;i<prices.size();i++){
+        //     result=prices[i];
+        //     for(int j=i+1;j<prices.size();j++){
+        //         if(prices[j]<=prices[i]){
+        //             result=prices[i]-prices[j];
+        //             break;
+        //         }
+        //     }
+        //     // result=min(m,result);
+        //     f.push_back(result);
+
+        // }
+        // return f;
+
+        int n=prices.size();
+        vector<int> v(prices);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && prices[st.top()]>=prices[i]){
+                v[st.top()]=prices[st.top()]-prices[i];
+                st.pop();
             }
-            // result=min(m,result);
-            f.push_back(result);
-
+            st.push(i);
         }
-        return f;
+        return v;
 
-        // vector<int> v;
-        // int n=
+
+
     }
 };
