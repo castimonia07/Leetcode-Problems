f8c5d6cb11f4e6e246f65ad3f27be728c5e6253c
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-05-30 12:35:26 +0530

    Time: 7 ms (68.62%) | Memory: 11.5 MB (83.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -14,16 +14,32 @@ public:
         // }
         // return max_len;
 
-        int maxlen=0;
+        // int maxlen=0;
+        // int n=s.size();
+        // for(int i=0;i<n;i++){
+        //     int hash[250]={0};
+        //     for(int j=i;j<n;j++){
+        //         if(hash[s[j]]==1) break;
+        //         int len=j-i+1;
+        //         maxlen=max(maxlen,len);
+        //         hash[s[j]]=1;
+        //     }
+        // }
+        // return maxlen;
+
+        vector<int> mpp(255,-1);
         int n=s.size();
-        for(int i=0;i<n;i++){
-            int hash[250]={0};
-            for(int j=i;j<n;j++){
-                if(hash[s[j]]==1) break;
-                int len=j-i+1;
-                maxlen=max(maxlen,len);
-                hash[s[j]]=1;
+        int l=0,r=0,maxlen=0;
+        while(r<n){
+            if(mpp[s[r]]!=-1){
+                if(mpp[s[r]]>=l){
+                    l=max(mpp[s[r]]+1,l);
+                }
             }
+            int len=r-l+1;
+            maxlen=max(len,maxlen);
+            mpp[s[r]]=r;
+            r++;
         }
         return maxlen;
     }
