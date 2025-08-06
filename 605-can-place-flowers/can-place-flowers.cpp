a5718525aeb5dfc401af567174dc6bf95aad2376
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-08-06 16:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (73.60%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int size = flowerbed.size();
-        if (n == 0) return true;
-        if (size == 1) {
-            return flowerbed[0] == 0 && n <= 1;
-        }
+    bool canPlaceFlowers(vector<int>& fb, int n) {
+        int size = fb.size();
         int cnt = 0;
-        // Check the first plot
-        if (flowerbed[0] == 0 && flowerbed[1] == 0) {
-            flowerbed[0] = 1;
-            cnt++;
-            if (cnt >= n) return true;
-        }
-        // Check middle plots
-        for (int i = 1; i < size - 1; i++) {
-            if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
-                flowerbed[i] = 1;
-                cnt++;
-                if (cnt >= n) return true;
+        for (int i = 0; i < size; i++) {
+            if (fb[i] == 0) {
+                bool emptyleft = (i == 0) || (fb[i - 1] == 0);
+                bool emptyright = (i == size - 1) || (fb[i + 1] == 0);
+                if (emptyleft && emptyright) {
+                    fb[i] = 1;
+                    cnt++;
+                }
             }
         }
-        // Check the last plot
-        if (flowerbed[size - 1] == 0 && flowerbed[size - 2] == 0) {
-            cnt++;
-            if (cnt >= n) return true;
-        }
-        return cnt >= n;
+        return (cnt >= n);
+
+        // int cnt=0;
+        // if(flowerbed.size()==1 && flowerbed[0]==0){
+        //     cnt++;
+        // }
+        // else if(flowerbed[0]==0&&flowerbed[1]==0){
+        //     cnt++;
+        //     flowerbed[0]=1;
+        // }
+        // for(int i=1;i<flowerbed.size()-1;i++){
+        //     if(flowerbed[i-1]==0&&flowerbed[i]==0&&flowerbed[i+1]==0){
+        //         cnt++;
+        //         flowerbed[i]=1;
+        //     }
+        // }
+        // int m=flowerbed.size();
+        // if(flowerbed[m-1]==0&&flowerbed[m-2]==0){
+        //     cnt++;
+        // }
+        // return cnt>=n;
     }
 };
