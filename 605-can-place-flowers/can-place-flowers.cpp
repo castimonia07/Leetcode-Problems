fb75651103e3baecd0a69791c4ff71e8ad70d4f8
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-08-06 16:57:08 +0530

    Time: 4 ms (6.87%) | Memory: 24.2 MB (5.75%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& fb, int n) {
-        int size = fb.size();
-        int cnt = 0;
-        for (int i = 0; i < size; i++) {
-            if (fb[i] == 0) {
-                bool emptyleft = (i == 0) || (fb[i - 1] == 0);
-                bool emptyright = (i == size - 1) || (fb[i + 1] == 0);
-                if (emptyleft && emptyright) {
-                    fb[i] = 1;
-                    cnt++;
-                }
-            }
-        }
-        return (cnt >= n);
-
-        // int cnt=0;
-        // if(flowerbed.size()==1 && flowerbed[0]==0){
-        //     cnt++;
-        // }
-        // else if(flowerbed[0]==0&&flowerbed[1]==0){
-        //     cnt++;
-        //     flowerbed[0]=1;
-        // }
-        // for(int i=1;i<flowerbed.size()-1;i++){
-        //     if(flowerbed[i-1]==0&&flowerbed[i]==0&&flowerbed[i+1]==0){
-        //         cnt++;
-        //         flowerbed[i]=1;
+        // int size = fb.size();
+        // int cnt = 0;
+        // for (int i = 0; i < size; i++) {
+        //     if (fb[i] == 0) {
+        //         bool emptyleft = (i == 0) || (fb[i - 1] == 0);
+        //         bool emptyright = (i == size - 1) || (fb[i + 1] == 0);
+        //         if (emptyleft && emptyright) {
+        //             fb[i] = 1;
+        //             cnt++;
+        //         }
         //     }
         // }
-        // int m=flowerbed.size();
-        // if(flowerbed[m-1]==0&&flowerbed[m-2]==0){
-        //     cnt++;
-        // }
-        // return cnt>=n;
+        // return (cnt >= n);
+
+        if(n==0) return true;
+        if(fb.size()==1 && fb[0]==0){
+            if(n<=1){
+                return true;
+            }
+            else return false;
+        }
+        int cnt=0;
+        if(fb[0]==0 && fb[1]==0){
+            cnt++;
+            fb[0]=1;
+        }
+        for(int i=1;i<fb.size()-1;i++){
+            if(fb[i-1]==0&&fb[i]==0&&fb[i+1]==0){
+                cnt++;
+                fb[i]=1;
+            }
+        }
+        int m=fb.size();
+        if(fb[m-1]==0&&fb[m-2]==0){
+            cnt++;
+        }
+        return cnt>=n;
     }
 };
