6493e440720db818864879651452c1ee05586b2f
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-08-08 12:45:02 +0530

    Time: 432 ms (6.54%) | Memory: 15.8 MB (50.05%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,11 +3,20 @@ public:
     int minMoves2(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int cnt=0,cnt1=0,cnt2=0;
-        int mid=n/2;
+        // int cnt=0,cnt1=0,cnt2=0;
+        // int mid=n/2;
+        // for(int i=0;i<n;i++){
+        //     cnt+=abs(nums[mid]-nums[i]);
+        // }
+        // return cnt;
+        long long res=INT_MAX;
         for(int i=0;i<n;i++){
-            cnt+=abs(nums[mid]-nums[i]);
+            long long sum=0;
+            for(int j=0;j<n;j++){
+                sum+=abs(nums[i]-nums[j]);
+            }
+            res=min(res,sum);
         }
-        return cnt;
+        return res;
     }
 };
