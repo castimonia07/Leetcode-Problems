ad4b51c6b6cefb5bb15514132e0cf5fb06a92319
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2025-08-30 01:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (60.74%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.cpp
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
         sort(costs.begin(),costs.end(),[](const vector<int>& a, const vector<int>& b){
-            return a[1]-a[0]<b[1]-b[0];
+            return a[0]-a[1]<b[0]-b[1];
         });
         int n=costs.size();
         int sum=0;
         for(int i=0;i<n/2;i++){
-            sum+=costs[i][1];
+            sum+=costs[i][0];
         }
         for(int i=n/2;i<n;i++){
-            sum+=costs[i][0];
+            sum+=costs[i][1];
         }
         return sum;
     }
