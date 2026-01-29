982b251c1048a44ea7591d53550aa9e257b80871
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-01-29 19:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,13 +8,16 @@ public:
     //     return dp[i]=left+right;
     // }
     int climbStairs(int n) {
-        vector<int> dp(n+1,0);
+        // vector<int> dp(n+1,0);
         // return climb(n,dp);
-        dp[1]=1,dp[0]=1;
+        int prev2=1;
+        int prev=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int cur=prev+prev2;
+            prev2=prev;
+            prev=cur;
         }
-        return dp[n];
+        return prev;
 
     }
 };
