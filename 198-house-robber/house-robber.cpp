4982cf929d6b39e2a898efbe82b9fa4383c9ebdf
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-01-29 20:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (78.38%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -12,15 +12,28 @@ public:
         int n=nums.size();
         // vector<int> dp(n,-1);
         // return func(nums,n-1,dp);
+        // if(n==1) return nums[0];
+        // vector<int> dp(n,0);
+        // dp[0]=nums[0];
+        // dp[1]=max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++){
+        //     int pick=nums[i]+dp[i-2];
+        //     int notpick=0+dp[i-1];
+        //     dp[i]=max(0+dp[i-1],nums[i]+dp[i-2]);
+        // }
+        // return dp[n-1];
+
         if(n==1) return nums[0];
-        vector<int> dp(n,0);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int pick=nums[i]+dp[i-2];
-            int notpick=0+dp[i-1];
-            dp[i]=max(0+dp[i-1],nums[i]+dp[i-2]);
+        int prev2=0;
+        int prev=nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            int nottake=0+prev;
+            int cur=max(take,nottake);
+            prev2=prev;
+            prev=cur;
         }
-        return dp[n-1];
+        return prev;
     }
 };
