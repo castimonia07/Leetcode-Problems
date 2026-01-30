a5d8f93b996be3f715a5a4db61d614e7ef7f38cd
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-01-30 14:30:59 +0530

    Time: 3 ms (59.59%) | Memory: 16.9 MB (52.45%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
+    // int func(int i,vector<int> &sum){
+    //     if(i==0) return sum[0];
+    //     if(i<0) return 0;
+    //     int pick=sum[i]+func(i-2,sum);
+    //     int notpick=0+func(i-1,sum);
+    //     return max(pick,notpick);
+    // }
     int deleteAndEarn(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
+        int n=nums.size();
+        if(n==1) return nums[0];
         unordered_map<int,int> freq;
         for(auto num:nums){
             freq[num]++;
@@ -14,6 +22,7 @@ public:
         for(auto it:freq){
             sum[it.first]=(it.first)*(it.second);
         }
+        // return func(maxval,sum);
         vector<int> dp(maxval+1,0); 
         dp[0]=0;
         dp[1]=sum[1];       
