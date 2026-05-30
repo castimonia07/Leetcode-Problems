9d3a48384a2333fc56f0941dcc2c2762cd7a4c15
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-05-30 09:27:11 +0530

    Time: 3 ms (29.66%) | Memory: 21.2 MB (18.27%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,54 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+        // int m=matrix.size();
+        // int n=matrix[0].size();
+        // vector<int> v1(m,0);
+        // vector<int>v2(n,0);
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             v1[i]=1;
+        //             v2[j]=1;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(v2[i]==1){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+        // for(int j=0;j<m;j++){
+        //     for(int i=0;i<n;i++){
+        //         if(v1[j]==1){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+        // return matrix;
 
-        vector<int> row(n,0);
-        vector<int> col(m,0);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        set<int> row;
+        set<int> column;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    row.insert(i);
+                    column.insert(j);
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1||col[j]==1){
-                    matrix[i][j]=0;
-                }
+        for(int i=0;i<matrix.size();i++){
+            for(auto st:column){
+                matrix[i][st]=0;
             }
         }
+        for(int i=0;i<matrix[0].size();i++){
+            for(auto st:row){
+                matrix[st][i]=0;
+            }
+        }
+        // return matrix;
     }
 };
