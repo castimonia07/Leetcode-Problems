96b71005d4f1e5ed0b90f82e66927c16a7a04c07
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-07-16 10:24:41 +0530

    Time: 5 ms (26.19%) | Memory: 9.8 MB (50.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t){return false;}
+        unordered_map<char,int> freq;
+        for(char c:s){
+            freq[c]++;
+        }
+        for(char c:t){
+            freq[c]--;
+            if(freq[c]==0){
+                freq.erase(c);
+            }
+        }
+        for(auto it: freq){
+            if(it.second!=0) return false;
+        }
         return true;
     }
 };
