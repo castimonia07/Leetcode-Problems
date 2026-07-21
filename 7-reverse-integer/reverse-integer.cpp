dda546a5b8b71bf3b1c65eb98051820b56887918
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-07-22 00:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (98.28%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,8 +5,8 @@ public:
         int last;
         while(x!=0){
             last=(x%10);
-            if(n>INT_MAX/10 || (n==INT_MAX/10 && last>7)) return 0;
-            if(n<INT_MIN/10 || (n==INT_MIN/10 && last<-87)) return 0;
+            if(n>INT_MAX/10 ) return 0;
+            if(n<INT_MIN/10 ) return 0;
             n=n*10+last;
             x=x/10;
         }
