dcdef071e565e3434586c1da7625fa644ea4b5c9
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-08-03 05:57:05 +0530

    Time: 6 ms (67.37%) | Memory: 19.1 MB (10.82%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // int n=s.length();
-        // int max_len=0;
-        // for(int i=0;i<n;i++){
-        //     unordered_set<char> seen;
-        //     for(int j=i;j<n;j++){
-        //         if(seen.count(s[j])) break;
-        //         seen.insert(s[j]);
-        //         max_len=max(max_len,j-i+1);
-        //     }
-
-        // }
-        // return max_len;
-
-        // int maxlen=0;
-        // int n=s.size();
-        // for(int i=0;i<n;i++){
-        //     int hash[250]={0};
-        //     for(int j=i;j<n;j++){
-        //         if(hash[s[j]]==1) break;
-        //         int len=j-i+1;
-        //         maxlen=max(maxlen,len);
-        //         hash[s[j]]=1;
-        //     }
-        // }
-        // return maxlen;
-
-        vector<int> mpp(255,-1);
-        int n=s.size();
-        int l=0,r=0,maxlen=0;
-        while(r<n){
-            if(mpp[s[r]]!=-1){
-                if(mpp[s[r]]>=l){
-                    l=max(mpp[s[r]]+1,l);
-                }
+        // vector<int> lower(26,0);
+        vector<int> hash(256,0);
+        int left=0;
+        int maxlen=0;
+        for(int right=left;right<s.size();right++){
+            // lower[s[right]-'a']++;
+            hash[s[right]]++;
+            while(hash[s[right]]>1){
+                // lower[s[left]-'a']--;
+                hash[s[left]]--;
+                left++;
             }
-            int len=r-l+1;
-            maxlen=max(len,maxlen);
-            mpp[s[r]]=r;
-            r++;
+            maxlen=max(maxlen,(right-left)+1);
         }
         return maxlen;
     }
