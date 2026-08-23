edf9e343d097057e5ba016726ac669405b353b0e
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-08-23 19:21:56 +0530

    Time: 23 ms (30.11%) | Memory: 16.6 MB (14.55%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(),people.end(),[](const vector<int>& a,const vector<int>& b){
+        sort(people.begin(),people.end(),[](const vector<int> &a,const vector<int> &b){
             if(a[0]==b[0]) return a[1]<b[1];
-            return a[0]>b[0];
+            else return a[0]>b[0];
         });
-        vector<vector<int>> v;
+
+        vector<vector<int>> ans;
         for(int i=0;i<people.size();i++){
-            vector<int> ppl=people[i];
-            v.insert(v.begin()+ppl[1],ppl);
+            vector<int> temp=people[i];
+            ans.insert(ans.begin()+temp[1],temp);
         }
-        return v;
+        return ans;
         
     }
 };
