cdacd17c4db98e5f25b97f039b8d8e377be7f349
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-08-24 22:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.38%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,9 +4,10 @@ public:
         int n=num.size();
         int i=0,j=n-1;
         while(j>=0){
-            if((num[j]-'0')%2==0) j--;
+            if((num[j]+'0')%2==0) j--;
             else break;
         }
         return num.substr(i,j-i+1);
     }
 };
+
