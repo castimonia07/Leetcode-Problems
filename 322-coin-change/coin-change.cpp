f093e70b777b08576b99ecb20f8384fc58370cdb
Rishabh Pandey <159942053+castimonia07@users.noreply.github.com>
2026-09-07 11:09:32 +0530

    Time: 45 ms (38.82%) | Memory: 49.3 MB (31.25%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int func(int i,int amount,vector<int>& coins,vector<vector<int>>& dp){
+    int func(vector<int> &coins,int amount,int i,vector<vector<int>> &dp){
+        if(amount==0) return 0;
         if(i==0){
-            if(amount%coins[i]==0){
-                return amount/coins[i];
-            }
+            if(amount%coins[i]==0) return amount/coins[i];
             else return 1e9;
         }
         if(dp[i][amount]!=-1) return dp[i][amount];
+
+        int not_take=func(coins,amount,i-1,dp);
         int take=1e9;
-        if(coins[i]<=amount) take=1+func(i,amount-coins[i],coins,dp);
-        int nottake=0+func(i-1,amount,coins,dp);
-        return dp[i][amount]=min(take,nottake);
+
+        if(amount>=coins[i]) take=1+func(coins,amount-coins[i],i,dp);
+
+        return dp[i][amount]=min(take,not_take);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=func(n-1,amount,coins,dp);
+        int ans=func(coins,amount,n-1,dp);
         if(ans>=1e9) return -1;
         return ans;
     }
